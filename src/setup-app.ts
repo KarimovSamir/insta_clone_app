daa523d3837340742cf14db1cdd5627ac1c6cd90
Samir Karimov <karimovsamir93@gmail.com>
2025-10-16 19:04:03 +0400

    little bug fix

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -25,7 +25,6 @@ import { blogRouter } from "./blogs/routers/blogs.router";
 import { postRouter } from "./posts/routers/posts.router";
 import { TESTING_PATH, BLOGS_PATH, POSTS_PATH, USERS_PATH } from "./core/paths/paths";
 import { dbReady } from "./db/mongo.db";
-import { errorsHandler } from "./core/errors/errors.handler";
 import { userRouter } from "./users/routers/users.router";
 
 export const setupApp = (app: Express) => {
@@ -51,7 +50,6 @@ export const setupApp = (app: Express) => {
   app.use(POSTS_PATH, postRouter);
   app.use(USERS_PATH, userRouter);
   app.use(TESTING_PATH, testingRouter);
-  app.use(errorsHandler);
 
   return app;
 };
