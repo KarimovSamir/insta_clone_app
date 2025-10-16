310e927b3fc6fbb932a735df515e9c177799536b
Samir Karimov <karimovsamir93@gmail.com>
2025-10-16 20:22:10 +0400

    testing.router

--- src/testing/routers/testing.router.ts
+++ src/testing/routers/testing.router.ts
@@ -1,6 +1,6 @@
 import { Router, Request, Response } from "express";
 import { HttpStatus } from "../../core/types/http-statuses";
-import { blogCollection, postCollection } from "../../db/mongo.db";
+import { blogCollection, postCollection, userCollection } from "../../db/mongo.db";
 
 export const testingRouter = Router({});
 
@@ -8,6 +8,7 @@ testingRouter.delete('/all-data', async (req: Request, res: Response) => {
   await Promise.all([
     postCollection.deleteMany(),
     blogCollection.deleteMany(),
+    userCollection.deleteMany(),
   ]);
   res.sendStatus(HttpStatus.NoContent);
 });
