7adbb6ab24adf15504fffb33592bedd86b0b7226
Samir Karimov <karimovsamir93@gmail.com>
2025-10-22 19:29:21 +0400

    index.d.ts add

--- src/auth/middlewares/bearer-auth.guard-middleware.ts
+++ src/auth/middlewares/bearer-auth.guard-middleware.ts
@@ -4,13 +4,13 @@ import { jwtService } from '../adapters/jwt.service';
 import { userCollection } from '../../db/mongo.db';
 import { ObjectId } from 'mongodb';
 
-declare global {
-    namespace Express {
-        interface Locals {
-            currentUser?: { _id: ObjectId; login: string; email: string; createdAt: string };
-        }
-    }
-}
+// declare global {
+//     namespace Express {
+//         interface Locals {
+//             currentUser?: { _id: ObjectId; login: string; email: string; createdAt: string };
+//         }
+//     }
+// }
 
 export const bearerAuthGuard = async (req: Request, res: Response, next: NextFunction) => {
     const auth = req.headers['authorization'];
---
+++ src/core/types/index.d.ts
@@ -0,0 +1,11 @@
+import { ObjectId } from "mongodb";
+
+declare global {
+    namespace Express {
+        interface Locals {
+            currentUser?: { _id: ObjectId; login: string; email: string; createdAt: string };
+        }
+    }
+}
+
+export {};
